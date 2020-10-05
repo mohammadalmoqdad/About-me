a8e58552da817da8f3d64c2d1d6f2f8953da40aa
mohammadalmoqdad <almekad.mohammad@gmail.com>
2020-10-06 01:58:26 +0300

    upload the pages

---
+++ js/myScript.js
@@ -0,0 +1,86 @@
+'user strict';
+
+var userName = prompt("Enter Your name plz");
+alert("lets dicover somethig about me " + userName + "!");
+
+var dogsAns = prompt("Do you think I love dogs? answer with y/n").toLowerCase();
+if (dogsAns == 'y' || 'yes') {
+
+    alert("Your answer is incorrect");
+}
+else if (dogsAns == 'no' || 'n') {
+    alert("Your answer is correct");
+}
+else {
+    alert("just enter y/n please!");
+}
+
+var scooolAns = prompt("have ever I been suspendeded at school ?\n answer with y/n ").toLowerCase();
+if (scooolAns == 'y' || 'yes') {
+    alert("Your answer is incorrect");
+
+}
+else if (scooolAns == 'no' || 'n') {
+    alert("Your answer is correct");
+}
+else {
+    alert("just enter y/n please!")
+}
+
+
+var loonlyAns = prompt("Do i hate Summer? \n answer with y/n").toLowerCase();
+if (loonlyAns == 'y' || 'yes') {
+    alert("Your answer is correct");
+
+}
+else if (loonlyAns == 'no' || 'n') {
+    alert("Your answer is incorrect");
+}
+else {
+    alert("just enter y/n please!")
+}
+
+
+
+var footballAns = prompt("Do I like watching football  \n answer with y/n").toLowerCase();
+if (footballAns == 'y' || 'yes') {
+    alert("Your answer is incorrect");
+
+}
+else if (footballAns == 'no' || 'n') {
+    alert("Your answer is correct");
+}
+else {
+    alert("just enter y/n please!")
+}
+
+
+
+var musicAns = prompt("Do I love music?  \n answer with y/n").toLowerCase();
+if (musicAns == 'y' || 'yes') {
+    alert("Your answer is incorrect");
+
+}
+else if (musicAns == 'no' || 'n') {
+    alert("Your answer is correct");
+}
+else {
+    alert("just enter y/n please!")
+}
+
+
+
+
+
+var photoAns = prompt("have i taken a wondorful picture?  \n answer with y/n").toLowerCase();
+if (photoAns == 'y' || 'yes') {
+    alert("Your answer is correct");
+
+}
+else if (photoAns == 'no' || 'n') {
+    alert("Your answer is incorrect");
+}
+else {
+    alert("just enter y/n please!")
+}
+
