a2940f24472c79869c5cc601728ebf2013f2a5b0
mohammadalmoqdad <almekad.mohammad@gmail.com>
2020-10-06 16:09:53 +0300

    add top 10 && lab2

--- js/myScript.js
+++ js/myScript.js
@@ -1,27 +1,29 @@
 'user strict';
-
+var count = 0;
 var userName = prompt("Enter Your name plz");
 alert("lets dicover somethig about me " + userName + "!");
 
 var dogsAns = prompt("Do you think I love dogs? answer with y/n").toLowerCase();
-if (dogsAns == 'y' || 'yes') {
+if (dogsAns == 'y' || dogsAns == 'yes') {
 
     alert("Your answer is incorrect");
 }
-else if (dogsAns == 'no' || 'n') {
+else if (dogsAns == 'no' || dogsAns == 'n') {
     alert("Your answer is correct");
+    count++;
 }
 else {
-    alert("just enter y/n please!");
+    alert("just enter y/n only!");
 }
 
 var scooolAns = prompt("have ever I been suspendeded at school ?\n answer with y/n ").toLowerCase();
-if (scooolAns == 'y' || 'yes') {
+if (scooolAns == 'y' || scooolAns == 'yes') {
     alert("Your answer is incorrect");
 
 }
-else if (scooolAns == 'no' || 'n') {
+else if (scooolAns == 'no' || scooolAns == 'n') {
     alert("Your answer is correct");
+    count++;
 }
 else {
     alert("just enter y/n please!")
@@ -29,11 +31,12 @@ else {
 
 
 var loonlyAns = prompt("Do i hate Summer? \n answer with y/n").toLowerCase();
-if (loonlyAns == 'y' || 'yes') {
+if (loonlyAns == 'y' || loonlyAns == 'yes') {
     alert("Your answer is correct");
+    count++;
 
 }
-else if (loonlyAns == 'no' || 'n') {
+else if (loonlyAns == 'no' || loonlyAns == 'n') {
     alert("Your answer is incorrect");
 }
 else {
@@ -43,12 +46,13 @@ else {
 
 
 var footballAns = prompt("Do I like watching football  \n answer with y/n").toLowerCase();
-if (footballAns == 'y' || 'yes') {
+if (footballAns == 'y' || footballAns == 'yes') {
     alert("Your answer is incorrect");
 
 }
-else if (footballAns == 'no' || 'n') {
+else if (footballAns == 'no' || footballAns == 'n') {
     alert("Your answer is correct");
+    count++;
 }
 else {
     alert("just enter y/n please!")
@@ -57,12 +61,13 @@ else {
 
 
 var musicAns = prompt("Do I love music?  \n answer with y/n").toLowerCase();
-if (musicAns == 'y' || 'yes') {
+if (musicAns == 'y' || musicAns == 'yes') {
     alert("Your answer is incorrect");
 
 }
-else if (musicAns == 'no' || 'n') {
+else if (musicAns == 'no' || musicAns == 'n') {
     alert("Your answer is correct");
+    count++;
 }
 else {
     alert("just enter y/n please!")
@@ -70,17 +75,58 @@ else {
 
 
 
+alert("Let us play a guessing game!");
+var guessNum;
+var i = 0;
 
+while (guessNum != 2 && i < 4) {
 
-var photoAns = prompt("have i taken a wondorful picture?  \n answer with y/n").toLowerCase();
-if (photoAns == 'y' || 'yes') {
-    alert("Your answer is correct");
+    guessNum = parseInt(prompt(" How many brothers do You think I have? "));
+    if (guessNum > 2 && guessNum != 3) {
+        alert("Too high");
+        if (i == 3) {
+            alert("The correct anwer is 2 :$");
+        }
+    }
+    else if (guessNum == 1 || guessNum == 3) {
+        alert("u are too close");
+        if (i == 3) {
+            alert("The correct anwer is 2 :$");
+        }
+
+    }
+    else if (guessNum < 2 && guessNum != 1) {
+        alert("too low!");
+        if (i == 3) {
+            alert("The correct anwer is 2 :$");
+        }
+    }
+    else if (guessNum == 2) {
+        alert("Correct!");
+        count++;
+
+        break;
+    }
+
+    i++;
 
 }
-else if (photoAns == 'no' || 'n') {
-    alert("Your answer is incorrect");
-}
-else {
-    alert("just enter y/n please!")
+
+var poets = ['الرافعي', 'المتنبي', 'قيس', 'عنترة', 'جرير', 'الفرزدق', 'المعري'];
+var guesspoem = prompt(" whom do you think is the arbic poet i like reading his poetry? \n (answer in Arabic please)");
+
+for (var y = 0; y < 6; y++) {
+    if (guesspoem == poets[0] || guesspoem == poets[1] || guesspoem == poets[2] || guesspoem == poets[3] || guesspoem == poets[4] || guesspoem == poets[5] || guesspoem == poets[6]) {
+        alert("Correct! You are a great predictor!");
+        count++;
+        break;
+    }
+    else {
+        alert("Incorrect answer. Try your best!");
+        guesspoem = prompt(" whom do you think is the arbic poet i like reading his poetry? \n (answer in Arabic please)");
+
+    }
 }
 
+alert("Those great poets are : " + 'الرافعي , المتنبي , قيس ,عنترة , جرير, الفرزدق, المعري');
+alert("Your total of correct answers is: " + count);
